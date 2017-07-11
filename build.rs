78ae4d01dacafcd34ce2e6e958039d9e1f88e1dc
Tuomo Jauhiainen <juppi@project-apollo.co.uk>
2017-07-11 17:48:35 +0300

    Enforce C99 for the HTTP library.

--- build.rs
+++ build.rs
@@ -2,8 +2,18 @@ extern crate gcc;
 
 fn main()
 {
-	gcc::compile_library(
-		"libhttpserver.a",
-		&["httpserver/httpsocket.c", "httpserver/httputils.c", "httpserver/httpserver.c"]
+	//println!("cargo:rustc-env=CFLAGS=-std=c99");
+
+	gcc::Config::new()
+		.flag("-std=gnu99")
+		.file("httpserver/httpsocket.c")
+		.file("httpserver/httputils.c")
+		.file("httpserver/httpserver.c")
+		.compile("libhttpserver.a"
 	);
+
+//		.compile_library(
+//			"libhttpserver.a",
+//			&["httpserver/httpsocket.c", "httpserver/httputils.c", "httpserver/httpserver.c"]
+//	);
 }
